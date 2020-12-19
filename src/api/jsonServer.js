fea3242897a677810139645b7e78e7fc7b681645
bazilucmarian <bazilucmarian98@yahoo.com>
2020-12-19 12:23:20 +0200

    first commit

---
+++ src/api/jsonServer.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+export default axios.create({
+  baseURL: "http://533fb7224e8f.ngrok.io",
+});
---
+++ src/screens/CreateScreen.js
@@ -0,0 +1,51 @@
+import React, { useState, useContext } from "react";
+import { View, Text, StyleSheet, TextInput, Button } from "react-native";
+import { Context } from "../context/BlogContext";
+
+const CreateScreen = ({ navigation }) => {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const { addBlogPost } = useContext(Context);
+
+  return (
+    <View>
+      <Text style={styles.label}>Enter title:</Text>
+      <TextInput
+        style={styles.input}
+        value={title}
+        onChangeText={(newText) => setTitle(newText)}
+      />
+      <Text style={styles.label}>Enter content:</Text>
+      <TextInput
+        style={styles.input}
+        value={content}
+        onChangeText={(newContent) => setContent(newContent)}
+      />
+      <Button
+        title="Add Blog Post"
+        onPress={() =>
+          addBlogPost(title, content, () => {
+            navigation.navigate("Index");
+          })
+        }
+      />
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  input: {
+    fontSize: 18,
+    borderWidth: 1,
+    borderColor: "black",
+    marginBottom: 15,
+    padding: 5,
+    margin: 5,
+  },
+  label: {
+    fontSize: 20,
+    marginBottom: 5,
+    marginLeft: 5,
+  },
+});
+
+export default CreateScreen;
